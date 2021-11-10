091930488a4a3782cd01315fe0da3aae53e5ef73
binmariney <binmariney@gmail.com>
2021-11-10 17:22:32 +0900

    router setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtieAYOprTr4oLEMKNd2WqMqp0eBBobp0",
+  authDomain: "twitter-clone-bd495.firebaseapp.com",
+  projectId: "twitter-clone-bd495",
+  storageBucket: "twitter-clone-bd495.appspot.com",
+  messagingSenderId: "390186404678",
+  appId: "1:390186404678:web:7703537303bfa1f28b3347"
+};
+
+export default firebase.initializeApp(firebaseConfig);
